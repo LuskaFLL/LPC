3752bb607db1ce9aba2b7d43c7934baa673570f7
Lucas Lima <76765014+LuskaFLL@users.noreply.github.com>
2021-09-24 22:45:18 -0300

    Add files via upload

---
+++ algoritimo.py
@@ -0,0 +1,255 @@
+print('Bem-vindo a bordo capitão :)\n Suas instruções são:\n "D" para decolar\n "A" para acelerar\n "P" para pousar\n '
+      '"F" para função piloto automático\n "L" para planar')
+acao = input('Digite sua ação: ')
+if acao == 'D':
+    print('\nO helicóptero decolou!!!')
+    acao = input('O que deseja fazer agora? ')
+    if acao == 'A':
+        print('\nO helicóptero está acelerando...')
+        acao = input('digite a próxima ação: ')
+        if acao == 'A':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'a':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'D':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'd':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'P':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'p':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'F':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'f':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'L':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'l':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        else:
+            print('\nnada aconteceu...')
+    elif acao == 'a':
+        print('\nO helicóptero está acelerando...')
+        acao = input('digite a próxima ação: ')
+        if acao == 'A':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'a':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'D':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'd':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'P':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'p':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'F':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'f':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'L':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'l':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        else:
+            print('\nnada aconteceu...')
+    elif acao == 'D':
+        print('\nO helicóptero já havia decolado...')
+    elif acao == 'd':
+        print('\nO helicóptero já havia decolado...')
+    elif acao == 'P':
+        print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+    elif acao == 'p':
+        print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+    elif acao == 'F':
+        print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+    elif acao == 'f':
+        print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+    elif acao == 'L':
+        print('O helicóptero está parado no ar...')
+        acao = input('digite a próxima ação: ')
+        if acao == 'A':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'a':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'D':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'd':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'P':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'p':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'F':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'f':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'L':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'l':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        else:
+            print('\nnada aconteceu...')
+    elif acao == 'l':
+        print('O helicóptero está parado no ar...')
+        acao = input('digite a próxima ação: ')
+        if acao == 'A':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'a':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'D':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'd':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'P':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'p':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'F':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'f':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'L':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'l':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        else:
+            print('\nnada aconteceu...')
+    else:
+        print('\nnada aconteceu...')
+elif acao == 'd':
+    print('\nO helicóptero decolou!!!')
+    acao = input('O que deseja fazer agora? ')
+    if acao == 'A':
+        print('\nO helicóptero está acelerando...')
+        acao = input('digite a próxima ação: ')
+        if acao == 'A':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'a':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'D':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'd':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'P':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'p':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'F':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'f':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'L':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'l':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        else:
+            print('\nnada aconteceu...')
+    elif acao == 'a':
+        print('\nO helicóptero está acelerando...')
+        acao = input('digite a próxima ação: ')
+        if acao == 'A':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'a':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'D':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'd':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'P':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'p':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'F':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'f':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'L':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'l':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        else:
+            print('\nnada aconteceu...')
+    elif acao == 'D':
+        print('\nO helicóptero já havia decolado...')
+    elif acao == 'd':
+        print('\nO helicóptero já havia decolado...')
+    elif acao == 'P':
+        print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+    elif acao == 'p':
+        print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+    elif acao == 'F':
+        print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+    elif acao == 'f':
+        print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+    elif acao == 'L':
+        print('O helicóptero está parado no ar...')
+        acao = input('digite a próxima ação: ')
+        if acao == 'A':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'a':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'D':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'd':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'P':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'p':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'F':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'f':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'L':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'l':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        else:
+            print('\nnada aconteceu...')
+    elif acao == 'l':
+        print('O helicóptero está parado no ar...')
+        acao = input('digite a próxima ação: ')
+        if acao == 'A':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'a':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'D':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'd':
+            print('\nO helicóptero já havia decolado...')
+        elif acao == 'P':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'p':
+            print('\nParabéns, você pousou o helicóptero com sucesso!!!')
+        elif acao == 'F':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'f':
+            print('\nPiloto automático ativado, o helicóptero irá pousar logo...\n \nO helicóptero pousou')
+        elif acao == 'L':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        elif acao == 'l':
+            print('\nA gasolina do helicóptero acabou, você agora está pousando...')
+        else:
+            print('\nnada aconteceu...')
+    else:
+        print('\nnada aconteceu...')
+elif acao == 'A':
+    print('\nVocê está em um helicóptero, não é possível acelerar no chão')
+elif acao == 'a':
+    print('\nVocê está em um helicóptero, não é possível acelerar no chão')
+elif acao == 'P':
+    print('\nÉ preciso decolar antes de tentar um pouso')
+elif acao == 'p':
+    print('\nÉ preciso decolar antes de tentar um pouso')
+elif acao == 'F':
+    print('\nPiloto automático ativado, o helicóptero decolou e irá pousar logo...\n \nO helicóptero pousou')
+elif acao == 'f':
+    print('\nPiloto automático ativado, o helicóptero decolou e irá pousar logo...\n \nO helicóptero pousou')
+elif acao == 'L':
+    print('\nNão é possível planar no chão')
+elif acao == 'l':
+    print('\nNão é possível planar no chão')
+else:
+    print('\nnada aconteceu...')
